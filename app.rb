726bae7e69d2c23eba0d77fa8316e446e963cb1e
JimPalo <jim.palo@elev.ga.ntig.se>
2025-11-21 14:20:20 +0100

    firstcommit

--- app.rb
+++ app.rb
@@ -6,10 +6,42 @@ require 'sinatra/reloader'
 
 
 
-
-# Routen /
-get '/' do
-    slim(:index)
+post('/todo/:id') do
+    todo_id = params[todo_id]
+    "You request ##{todo_id}"
 end
+
+
+get('/todoslist') do
+
+  query = params[:q]
+  p "Yo skrev #{query}"
+
+  #gör koppling till db
+  db = SQLite3::Database.new("db/todos.db")
+
+  #[{},{},{}] önskar vi oss istället för [[], [],[]]
+  db.results_as_hash = true
+
+  #hämta allting från db
+  @datatodo = db.execute("SELECT * FROM todoslist")    
+
+  p @datatodo
+
+  if query && !query.empty?
+    @datatodo = db.execute("SELECT * FROM todoslist WHERE name LIKE ?","%#{query}%")
+  else
+    @datatodo = db.execute("SELECT * FROM todoslist")
+  end
+
+  #visa med slim
+  slim(:"todoslist/index")
+
+
+
+end
+
+
+
 
 

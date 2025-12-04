a6d3119b4e0532fb179368ea1e257d3c341e5f74
JimPalo <jim.palo@elev.ga.ntig.se>
2025-12-04 16:51:58 +0100

    gjort done delen

--- app.rb
+++ app.rb
@@ -24,9 +24,10 @@ end
 post('/todo') do
   new_todo = params[:name]
   new_description = params[:description]
+  state = false
 
   db = SQLite3::Database.new("db/todos.db")
-  db.execute("INSERT INTO todos (name, description) VALUES (?, ?)", [new_todo, new_description])
+  db.execute("INSERT INTO todos (name, description, state) VALUES (?, ?, ?)", [new_todo, new_description, state])
 
   redirect('/todos')
 end
@@ -46,9 +47,11 @@ post('/todos/:id/update') do
   id = params[:id].to_i
   name = params[:name]
   description = params[:description]
+  state = params[:state]
+  
 
   db = SQLite3::Database.new("db/todos.db")
-  db.execute("UPDATE todos SET name=?, description=? WHERE id=?",[name,description,id])
+  db.execute("UPDATE todos SET name=?, description=?, state=? WHERE id=?",[name,description,state, id])
   redirect('/todos')
 end
 
@@ -61,9 +64,29 @@ post('/todos/:id/delete') do
   redirect('/todos')
 end
 
+post '/todos/:id/state' do
+  id = params[:id].to_i
+
+
+  db = SQLite3::Database.new("db/todos.db")
+  db.results_as_hash = true
+
+  current = db.execute("SELECT state FROM todos WHERE id=?", [id]).first["state"]
+  new_state = current == 0 ? 1 : 0
+
+  db.execute("UPDATE todos SET state=? WHERE id=?", [new_state, id])
+
+  redirect('/todos')
+end
+
+post ''
+
+
 get('/todos/new') do
   slim(:"todos/new")
 end
+
+
 
 
 
